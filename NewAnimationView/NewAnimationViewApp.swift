42836ec09fdc834ff1cf91676e8ed74e0c0edf7c
심상갑 <kappa2410@gmail.com>
2024-02-23 13:21:01 +0900

    Initial Commit

---
+++ NewAnimationView/NewAnimationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewAnimationViewApp.swift
+//  NewAnimationView
+//
+//  Created by Gab on 2024/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NewAnimationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
